4f6e604053bc4a7183ac41203af89487d7857d94
JoelAPL <joel.ale.programmer@gmail.com>
2023-09-18 22:50:50 -0500

    favicon error

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <!-- Compressed CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/foundation-sites@6.8.1/dist/css/foundation.min.css" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
-    <link rel="icon" href="/calculadora-PHP/favicor.svg">
+    <link rel="icon" href="/calculadora-PHP/favicon.svg">
     <title>Calculadora PHP</title>
 </head>
 <body>
